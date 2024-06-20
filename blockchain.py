58ebb9f50ce833d24fb6f2ee6af3599a3fbb33c3
Nicholas Santos <santos.nick27@gmail.com>
2024-06-19 22:13:19 -0400

    Started function to add transactions

--- blockchain.py
+++ blockchain.py
@@ -91,5 +91,44 @@ class Blockchain(object):
 
 # todo: setup python flask
 # /transactions/new -> creates new transaction to a block
+    
+# instantiate new node    
+app = Flask(__name__)
+# generate globally unique address for the node
+node_identifier = str(uuid4()).replace('-', '')
+
+# instantiate blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "Mining a new block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # check that required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    # create a new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to block {index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
 # /mine             -> tells server to mine a new block
 # /chain            -> returns full Blockchain
